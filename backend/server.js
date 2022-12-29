5656223d8fa29d4eaf3ce4d899323b5eaf525fd5
Michael Loughman <mcloughman@gmail.com>
2022-12-28 21:47:14 -0800

    Initialize user backend models, routes and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const mongoose = require("mongoose")
 
 const postRoutes = require("./routes/posts")
+const userRoutes = require("./routes/user")
 
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/posts', postRoutes)
+app.use('/api/user', userRoutes)
 
 mongoose.set('strictQuery', false)
 
