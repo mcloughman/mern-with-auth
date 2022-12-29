f388f44ac8a4a4810bc69e32211034b91df158b9
Michael Loughman <mcloughman@gmail.com>
2022-12-29 15:05:14 -0800

    Add static login method to our userModel and implement it in userController. Test for functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,15 @@ const createToken = (_id) => {  //making a reusable function that generates toke
 
 // login user
 const loginUser = async(req, res) => {
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    } catch(error) {
+        res.status(400).json({error: error.message})
+        console.log(error)
+    }
 }
 
 // signup user
